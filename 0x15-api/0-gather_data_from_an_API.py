6ab91ce8b3eb1045a24797bcb1aa047f6cc6469a
azukauteh <aazure263@gmail.com>
2023-10-23 12:59:11 +0200

    Python script that, using this REST API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+            print("Usage: python3 0-gather_data_from_an_API.py <user_id>")
+            sys.exit(1)
+
+            url = "https://jsonplaceholder.typicode.com/"
+            user_id = sys.argv[1]
+            user = requests.get(url + "users/{}".format(user_id)).json()
+            todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+            completed = [t.get("title") for t in todos if t.get("completed") is True]
+            print("Employee {} is done with tasks({}/{}):".format(
+                user.get("name"), len(completed), len(todos)))
+            [print("\t {}".format(c)) for c in completedi
