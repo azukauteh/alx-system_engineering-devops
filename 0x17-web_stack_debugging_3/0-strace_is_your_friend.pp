79d57a565eaf9d97f5d6c178855103d661b6d4b4
Azuka uteh <aazure263@gmail.com>
2023-11-11 01:03:24 +0000

    Apache is returning a 500 error!

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,6 @@
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Notify that we are fixing Apache
-notify { 'Fixing Apache':
-  message => 'Fixing Apache to resolve 500 Internal Server Error',
-}
-
-# Define an exec resource to fix the identified issue
-exec { 'fix-apache-issue':
-  command     => '/path/to/your/fix_script.sh', # Replace with the actual fix script
-  refreshonly => true,
-  subscribe   => Notify['Fixing Apache'],
-}
-
-# Restart Apache after fixing the issue
-service { 'apache2':
-  ensure  => 'running',
-  require => Exec['fix-apache-issue'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
